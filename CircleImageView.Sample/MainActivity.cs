63e694719ff56fed4159fa2f39ebfb8398596718
Bradley Locke <brad.locke@gmail.com>
2016-01-07 15:12:54 +1100

    * README.md: * screenshot.png: * packages.config: * BaseActivity.cs: * MainActivity.cs: * packages.config: * MainActivity.cs: * CircleImageView.cs: * AboutAssets.txt: * main.axml: * AssemblyInfo.cs: * Main.axml: * colors.xml: * styles.xml: * attrs.xml: * Strings.xml: * homer.jpg: * AboutResources.txt: * toolbar.xml: * Strings.xml: * AboutResources.txt: * Sample.CircleImageView.csproj: * AndroidManifest.xml: * Resource.Designer.cs: * Icon.png: * Icon.png: * profile.png: * Resource.designer.cs: * AndroidManifest.xml: * Icon.png: * Resource.designer.cs: * CircleImageView.Xamarin.csproj: * Icon.png: * Icon.png:
    * CircleImageView.Sample.csproj: Updated to version 2
    Changed namespace

--- CircleImageView.Sample/MainActivity.cs
+++ CircleImageView.Sample/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Graphics;
 using Android.OS;
+using civ.Xamarin;
 
 namespace CircleImageView.Sample
 {
@@ -16,16 +17,25 @@ namespace CircleImageView.Sample
         {
             base.OnCreate(bundle);
 
-            var profileimage = FindViewById<Xamarin.CircleImageView>(Resource.Id.profile_image);
-            var largeprofileimage = FindViewById<Xamarin.CircleImageView>(Resource.Id.large_profile_image);
+			SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+			SupportActionBar.SetHomeButtonEnabled(false);
+
+			var profileimage = FindViewById<civ.Xamarin.CircleImageView>(Resource.Id.profile_image);
+			var largeprofileimage = FindViewById<civ.Xamarin.CircleImageView>(Resource.Id.large_profile_image);
+			var fillimage = FindViewById<civ.Xamarin.CircleImageView>(Resource.Id.fill_image);
 
             largeprofileimage.SetImageResource(Resource.Drawable.homer);
             largeprofileimage.SetBorderColor(Color.Blue);
             largeprofileimage.SetBorderWidth(20);
             largeprofileimage.SetBorderOverlay(true);
 
-            SupportActionBar.SetDisplayHomeAsUpEnabled(false);
-            SupportActionBar.SetHomeButtonEnabled(false);
+			fillimage.SetImageResource(Resource.Drawable.profile);
+			fillimage.SetFillColor (Color.OrangeRed);
+			fillimage.SetBorderColor (Color.ForestGreen);
+			fillimage.SetBorderWidth (10);
+			fillimage.SetBorderOverlay (true);
+
+
         }
     }
 }
